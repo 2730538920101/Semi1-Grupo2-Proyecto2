dbebace415f208b0d9b9fca3238bc2a1ac21a294
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-10-31 12:03:30 -0600

    [ADD] Endpoint que permite visualizar todos los post que se han realizado con sus respectivos comentarios

--- backend/src/post.js
+++ backend/src/post.js
@@ -32,4 +32,29 @@ router.post('', bucket.upload.single('APP_POST_IMAGE'), async (req, res) => {
     }
 });
 
+// Obtener todos los post con sus respectivos comentarios
+router.get('', (req, res) => {
+    const commentId = req.params.id;
+    const query = `SELECT ap.APP_POST_ID AS ID_POST, ap.APP_POST_IMAGE AS IMAGE_POST, ap.APP_POST_DESCRIPTION, ap.APP_POST_DATE,
+    au.ID_USER AS ID_USER_POST, au.FULL_NAME AS FULL_NAME_USER_POST, au.PICTURE AS IMAGE_USER_POST,
+    JSON_ARRAYAGG(JSON_OBJECT(
+    'APP_COMENT_ID', ac.APP_COMENT_ID ,'COMENT', ac.COMENT, 'APP_COMENT_DATE', ac.APP_COMENT_DATE,
+    'ID_USER_COMENT', au2.ID_USER, 'FULL_NAME_USER_COMENT', au2.FULL_NAME, 'IMAGE_USER_COMENT', au2.PICTURE
+    )) AS COMENTS
+    FROM APP_POST ap
+    INNER JOIN APP_USER au ON au.ID_USER = ap.APP_USER_ID
+    LEFT JOIN APP_COMENT ac ON ac.APP_POSTED_ID = ap.APP_POST_ID
+    LEFT JOIN APP_USER au2 ON au2.ID_USER = ac.APP_SENDER_ID
+    GROUP BY ID_POST
+    ORDER BY ap.APP_POST_DATE DESC;`;
+    conn.query(query, [commentId], (err, result) => {
+        if (err) {
+            console.error('Error al obtener los post:', err);
+            res.json({ success: false, result: "Ha ocurrido un error al obtener los post" });
+        } else {
+            res.json({ success: true, result: result });
+        }
+    });
+});
+
 module.exports = router;
